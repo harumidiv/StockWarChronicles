c208d97f656fe8d35a8525163191db24a7a49ebf
harumidiv <h_sagawa@yumemi.co.jp>
2025-09-01 23:07:39 +0900

    保存ボタンを配置して一気に保存する形に変更

--- StockWarChronicles/Screen/Edit/EditScreen.swift
+++ StockWarChronicles/Screen/Edit/EditScreen.swift
@@ -8,45 +8,115 @@
 import SwiftUI
 import SwiftData
 
-// 編集画面
 struct EditScreen: View {
     @Bindable var record: StockRecord
+    @Environment(\.modelContext) private var context
     @Environment(\.dismiss) private var dismiss
+    
+    @State private var code: String = ""
+    @State private var market: Market = .tokyo
+    @State private var name: String = ""
+    @State private var date: Date = Date()
+    @State private var amountText: String = ""
+    @State private var sharesText: String = ""
+    @State private var reason: String = ""
+    @State private var selectedTags: [CategoryTag] = []
+    
+    @State private var keyboardIsPresented: Bool = false
     
     var body: some View {
         NavigationView {
-            StockFormView(
-                code: $record.code,
-                market: $record.market,
-                name: $record.name,
-                date: $record.purchase.date,
-                amountText: Binding(
-                    get: { String(record.purchase.amount) },
-                    set: { record.purchase.amount = Double($0) ?? 0 }
-                ),
-                sharesText: Binding(
-                    get: { String(record.purchase.shares) },
-                    set: { record.purchase.shares = Int($0) ?? 0 }
-                ),
-                reason: $record.purchase.reason,
-                selectedTags: Binding(
-                    get: { record.tags.map { .init(name: $0.name, color: $0.color) } },
-                    set: { newTags in
-                        record.tags = newTags.map { Tag(categoryTag: $0) }
-                    }
+            VStack {
+                StockFormView(
+                    code: $code,
+                    market: $market,
+                    name: $name,
+                    date: $date,
+                    amountText: $amountText,
+                    sharesText: $sharesText,
+                    reason: $reason,
+                    selectedTags: $selectedTags
                 )
-            )
+                
+                if keyboardIsPresented {
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Button {
+                                saveChanges()
+                            } label: {
+                                Label("保存", systemImage: "square.and.arrow.down")
+                                    .foregroundColor(.blue)
+                                    .padding()
+
+                            }
+                        }
+                        .padding(.horizontal)
+                        .background(.ultraThinMaterial)
+                        
+                    }
+                    .background(Color.clear)
+                }
+                
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { _ in
+                keyboardIsPresented = true
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)) { _ in
+                keyboardIsPresented = false
+            }
+            
             .navigationTitle("編集")
             .toolbar {
+                
                 ToolbarItem(placement: .topBarTrailing) {
-                    Button("dismiss", systemImage: "xmark") {
+                    Button {
                         dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                    }
+                }
+                
+                ToolbarSpacer(.flexible, placement: .bottomBar)
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        // TOOD: ここで売りの株数が買いの株数よりも多い場合はアラート
+                        
+                        saveChanges()
+                    } label: {
+                        Image(systemName: "square.and.arrow.down")
+                            .foregroundColor(.blue)
                     }
                 }
             }
         }
+        .onAppear {
+            code = record.code
+            market = record.market
+            name = record.name
+            date = record.purchase.date
+            amountText = String(record.purchase.amount)
+            sharesText = String(record.purchase.shares)
+            reason = record.purchase.reason
+            selectedTags = record.tags.map { .init(name: $0.name, color: $0.color) }
+        }
+    }
+    
+    private func saveChanges() {
+        record.code = code
+        record.market = market
+        record.name = name
+        record.purchase.date = date
+        record.purchase.amount = Double(amountText) ?? 0
+        record.purchase.shares = Int(sharesText) ?? 0
+        record.purchase.reason = reason
+        record.tags = selectedTags.map { .init(name: $0.name, color: $0.color) }
+        
+        try? context.save()
+        dismiss()
     }
 }
+
 
 
 #Preview {
