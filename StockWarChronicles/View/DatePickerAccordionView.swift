b28d6c9b15c3b514a905ada7d31a729fa2b06f79
harumidiv <h_sagawa@yumemi.co.jp>
2025-10-02 14:41:18 +0900

    DatePickerをポップアップで表示すると閉じる時に不自然で使いにくいので自作

---
+++ StockWarChronicles/View/DatePickerAccordionView.swift
@@ -0,0 +1,43 @@
+//
+//  DatePickerAccordionView.swift
+//  StockWarChronicles
+//
+//  Created by 佐川 晴海 on 2025/10/02.
+//
+
+import SwiftUI
+
+struct DatePickerAccordionView: View {
+    @Binding var date: Date
+    
+    @State private var isExpanded: Bool = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("日付")
+                Spacer()
+                Button(action: {
+                    withAnimation {
+                        isExpanded.toggle()
+                    }
+                }) {
+                    Text(date.formatted(as: .yyyyMMdd))
+                            .padding(.horizontal, 8)
+                            .padding(.vertical, 4)
+                            .foregroundColor(isExpanded ? .blue : .primary)
+                            .background(Color(.systemGray5))
+                            .cornerRadius(100)
+                }
+            }
+            if isExpanded {
+                DatePicker("", selection: $date, displayedComponents: .date)
+                    .datePickerStyle(.graphical)
+            }
+        }
+    }
+}
+
+#Preview {
+    DatePickerAccordionView(date: .constant(Date()))
+}
